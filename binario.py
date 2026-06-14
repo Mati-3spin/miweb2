54c3fddd877da26e6686d94c49c5b20930b0bad3
Mati-3spin <mespinoza2026@alu.uct.cl>
2026-06-14 13:20:03 -0400

    binario.py
    En el caso de agregar numeros mayores a 1 los considera 0

--- binario.py
+++ binario.py
@@ -1,7 +1,7 @@
 print("Convertidor de binario a decimal")
 prefijo = [128,64,32,16,8,4,2,1]
 binario = input('Ingrese un número binario:')
-while len(binario) != 8 and '0' or '1' not in binario:
+while len(binario) != 8 and ('0' or '1') not in binario:
     print("Debe tener 8 prefijos (solo transforma a numeros decimales del 0 al 255)")
     binario = input('Ingrese un número binario:')
 número = 0
